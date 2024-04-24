5ec777b66a954827cfde9d32eb818478f2c6e14c
Janar Rahumeel <janar.rahumeel@gmail.com>
2024-04-24 18:58:39 +0300

    [RIK-1] Create project skeleton

---
+++ backend/src/main/java/ee/rik/RikHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ee.rik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RikHomeworkApplication.class);
+    }
+
+}
